9b0e54b245ea88e1ada14cc68cf48f8ce579d252
LeithPrice <leith.price@gmail.com>
2022-10-24 12:18:49 +1000

    created user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope '/api' do
-    resources: :user, param: :_username
+    # resources: :user, param: :_username
     get '/bookings', to: 'bookings#index'
     post '/bookings', to: 'bookings#create'
     get '/bookings/:id', to: 'bookings#show'
