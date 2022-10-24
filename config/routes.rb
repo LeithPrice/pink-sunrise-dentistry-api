b4c012b3e6e6b309ad0515ffc995c5b150ddcafe
LeithPrice <leith.price@gmail.com>
2022-10-24 11:58:09 +1000

    installed jwt and bcrypt ruby gem, and wrote Jsonwebtoken file

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope '/api' do
+    resources: :user, param: :_username
     get '/bookings', to: 'bookings#index'
     post '/bookings', to: 'bookings#create'
     get '/bookings/:id', to: 'bookings#show'
     put '/bookings/:id', to: 'bookings#update'
     delete '/bookings/:id', to: 'bookings#destroy'
+  end
+  scope '/auth' do
+    post '/login', to: 'authentication#login'
   end
 end
