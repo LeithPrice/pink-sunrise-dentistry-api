1850ab1b0d009af2f357de8437864568b44edfea
LeithPrice <leith.price@gmail.com>
2022-10-24 10:29:02 +1000

    created booking model DB and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope '/api' do
+    get '/bookings', to: 'bookings#index'
+    post '/bookings', to: 'bookings#create'
+    get '/bookings/:id', to: 'bookings#show'
+    put '/bookings/:id', to: 'bookings#update'
+    delete '/bookings/:id', to: 'bookings#destroy'
+  end
 end
