ce6fcadf123494f47214e0fd0ada57aafc329f9f
LeithPrice <leith.price@gmail.com>
2022-10-24 12:42:53 +1000

    created authenticatio_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_001335) do
+ActiveRecord::Schema.define(version: 2022_10_24_021510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2022_10_24_001335) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "appointment_time"
     t.text "doctor_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "user_type"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
