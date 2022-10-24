9255130e873584d8ecbfbcb65d1d431bd5cdbcb6
LeithPrice <leith.price@gmail.com>
2022-10-24 11:31:41 +1000

    completed initial CRUD methods in bookings_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
+    before_action : set_booking, only: [:show, :update, :destroy]
+    
     def index
         @booking = Booking.all
         render json: @booking
@@ -13,8 +15,33 @@ class BookingsController < ApplicationController
         end
     end
 
+    def show
+        render json: @booking
+    end
+
+    def update
+        @booking.update(booking_params)
+        if @booking.errors.any?
+            render json: @booking.errors, status: :unprocessible_entity
+        else
+            render json: @booking, status: 201
+        end
+    end
+
+    def destroy
+        @booking.delete
+        render json: 204
+
     private
     def booking_params
         params.require(:booking).permit(:appointment_time, :doctor_id)
+    end
+
+    def set_booking
+        begin
+            @booking = Booking.find(params[:id])
+        rescue 
+            render json: {error: "Booking not found"}, status: 404
+        end
     end
 end
