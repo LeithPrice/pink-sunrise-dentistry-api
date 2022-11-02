a72a5b1fdfe1ddcff6566057c3d5a4c6ab71366a
LeithPrice <leith.price@gmail.com>
2022-11-02 23:23:17 +1000

    5th attempt

--- db/migrate/20221102104859_remove_doctor_from_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDoctorFromBookings < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :bookings, :doctor_id
-  end
-end
--- db/migrate/20221102110922_remove_user_from_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromBookings < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :bookings, :user_id
-  end
-end
