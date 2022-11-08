063593d786e4d3a5b91b7f6afacc8c9ea33fdbf1
LeithPrice <leith.price@gmail.com>
2022-11-08 20:57:53 +1000

    cleaned up migrations ready fo rnew rear api

--- db/migrate/20221102111651_add_user_and_doctor_to_booking.rb
+++
@@ -1,6 +0,0 @@
-class AddUserAndDoctorToBooking < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :bookings, :doctor, foreign_key: { to_table: :users }
-    add_reference :bookings, :patient, foreign_key: { to_table: :users }
-  end
-end
